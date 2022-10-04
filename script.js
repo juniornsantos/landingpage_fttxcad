5c494f67cf30063a532c971dd059f3820b91e102
Junior Nogueira <frjuniornogueira1@gmail.com>
2022-10-04 12:24:10 -0300

    iniciando home

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // animação de texto
     var typed = new Typed(".typing", {
-        strings: ["Software Developer"],
+        strings: ["Projetos FTTx direto no AutoCAD"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Software Developer"],
+        strings: ["Projetos FTTx direto no AutoCAD"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
