46dcab0e941dd6e46518ef33e824d32489f24a8a
Junior Nogueira <frjuniornogueira1@gmail.com>
2022-10-05 14:54:25 -0300

    .navbar e home relativamentes prontos

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // animação de texto
     var typed = new Typed(".typing", {
-        strings: ["Projetos FTTx direto no AutoCAD"],
+        strings: ["Tenha uma visão perfeita de seus projetos."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Projetos FTTx direto no AutoCAD"],
+        strings: ["Tenha uma visão perfeita de seus projetos."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
